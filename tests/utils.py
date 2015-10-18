3de7c2e34d231ede249af0cffa8176401633089b
Daira Hopwood <daira@jacaranda.org>
2015-10-19 00:24:21 +0100

    Allow tests to read/write files in binary mode, and to expect an error return from main.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- tests/utils.py
+++ tests/utils.py
@@ -7,7 +7,7 @@ import shutil
 from libmodernize.main import main as modernize_main
 
 
-def check_on_input(input_content, expected_content, extra_flags = []):
+def check_on_input(input_content, expected_content, extra_flags=[], mode="t"):
     """
     Check that input_content is fixed to expected_content, idempotently.
 
@@ -19,14 +19,16 @@ def check_on_input(input_content, expected_content, extra_flags = []):
     tmpdirname = tempfile.mkdtemp()
     try:
         test_input_name = os.path.join(tmpdirname, "input.py")
-        with open(test_input_name, "wt") as input_file:
+        with open(test_input_name, "w" + mode) as input_file:
             input_file.write(input_content)
 
         def _check(this_input_content, which_check):
-            modernize_main(extra_flags + ["-w", test_input_name])
+            ret = modernize_main(extra_flags + ["-w", test_input_name])
+            if ret != 0:
+                raise AssertionError("didn't expect to fail (returned %r)" % (ret,))
 
             output_content = ""
-            with open(test_input_name, "rt") as output_file:
+            with open(test_input_name, "r" + mode) as output_file:
                 for line in output_file:
                     if line:
                         output_content += line
@@ -38,5 +40,19 @@ def check_on_input(input_content, expected_content, extra_flags = []):
         _check(input_content, "output check failed")
         if input_content != expected_content:
             _check(expected_content, "idempotence check failed")
+    finally:
+        shutil.rmtree(tmpdirname)
+
+
+def expect_error(input_content, extra_flags=[], mode="t"):
+    tmpdirname = tempfile.mkdtemp()
+    try:
+        test_input_name = os.path.join(tmpdirname, "input.py")
+        with open(test_input_name, "w" + mode) as input_file:
+            input_file.write(input_content)
+
+        ret = modernize_main(extra_flags + ["-w", test_input_name])
+        if ret == 0:
+            raise AssertionError("didn't expect to succeed")
     finally:
         shutil.rmtree(tmpdirname)
