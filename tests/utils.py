17e9b515493d0cdb07480aab31f592c8bf040d29
Daira Hopwood <daira@jacaranda.org>
2015-10-19 11:33:31 +0100

    Allow tests to read/write files in binary mode, and to expect an error return from main.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import sys
 import os.path
 import tempfile
 import shutil
@@ -7,7 +8,7 @@ import shutil
 from libmodernize.main import main as modernize_main
 
 
-def check_on_input(input_content, expected_content, extra_flags = []):
+def check_on_input(input_content, expected_content, extra_flags=[], mode="t"):
     """
     Check that input_content is fixed to expected_content, idempotently.
 
@@ -19,24 +20,41 @@ def check_on_input(input_content, expected_content, extra_flags = []):
     tmpdirname = tempfile.mkdtemp()
     try:
         test_input_name = os.path.join(tmpdirname, "input.py")
-        with open(test_input_name, "wt") as input_file:
+        with open(test_input_name, "w" + mode) as input_file:
             input_file.write(input_content)
 
         def _check(this_input_content, which_check):
-            modernize_main(extra_flags + ["-w", test_input_name])
+            ret = modernize_main(extra_flags + ["-w", test_input_name])
+            if ret != 0:
+                raise AssertionError("didn't expect to fail (returned %r)" % (ret,))
 
-            output_content = ""
-            with open(test_input_name, "rt") as output_file:
-                for line in output_file:
-                    if line:
-                        output_content += line
+            with open(test_input_name, "r" + mode) as output_file:
+                if mode == "b":
+                    output_content = output_file.read()
+                else:
+                    output_content = "".join([line for line in output_file if line])
 
             if output_content != expected_content:
-                raise AssertionError("%s\nInput:\n%sOutput:\n%s\nExpecting:\n%s" %
+                fmt = "%r" if mode == "b" else "%s"
+                raise AssertionError(("%s\nInput:\n"+fmt+"\nOutput:\n"+fmt+"\nExpecting:\n"+fmt) %
                                      (which_check, this_input_content, output_content, expected_content))
 
         _check(input_content, "output check failed")
         if input_content != expected_content:
             _check(expected_content, "idempotence check failed")
+    finally:
+        shutil.rmtree(tmpdirname)
+
+
+def expect_error(input_content, extra_flags=[], mode="t"):
+    tmpdirname = tempfile.mkdtemp()
+    try:
+        test_input_name = os.path.join(tmpdirname, "input.py")
+        with open(test_input_name, "w" + mode) as input_file:
+            input_file.write(input_content)
+
+        ret = modernize_main(extra_flags + ["-w", test_input_name])
+        if ret == 0:
+            raise AssertionError("didn't expect to succeed")
     finally:
         shutil.rmtree(tmpdirname)
