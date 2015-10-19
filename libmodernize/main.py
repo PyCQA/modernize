c3fb1b7e20a87ff881d95f430cb6c1e8fe575711
Thomas Kluyver <takowl@gmail.com>
2015-10-19 10:43:10 +0100

    Add machinery to rewrite line endings after writing a file

--- libmodernize/main.py
+++ libmodernize/main.py
@@ -7,6 +7,7 @@ Python           _              _
 
 from __future__ import absolute_import, print_function
 
+import io
 import sys
 import logging
 import optparse
@@ -16,6 +17,28 @@ from lib2to3 import refactor
 
 from libmodernize import __version__
 from libmodernize.fixes import lib2to3_fix_names, six_fix_names, opt_in_fix_names
+
+
+class LineEndingsRefactoringTool(StdoutRefactoringTool):
+    '''2to3 refactoring tool that rewrites files with specified line endings'''
+    def __init__(self, *args, newline=None, **kwargs):
+        super(LineEndingsRefactoringTool, self).__init__(*args, **kwargs)
+        self.newline = newline
+
+    def write_file(self, new_text, filename, old_text, encoding):
+        super(LineEndingsRefactoringTool, self).write_file(new_text, filename,
+                                                           old_text, encoding)
+
+        if self.newline is not None:
+            self.log_debug("Rewriting %s with line endings %r",
+                           filename, self.newline)
+            with io.open(filename, 'r', encoding=encoding) as f:
+                contents = f.read()
+
+            with io.open(filename, 'w', encoding=encoding,
+                         newline=self.newline) as f:
+                 f.write(contents)
+
 
 usage = __doc__ + """\
  %s
@@ -86,6 +109,10 @@ def main(args=None):
             print(fixname)
         if not args:
             return 0
+    if options.unix_line_endings and options.windows_line_endings:
+        print("--unix-line-endings and --windows-line-endings are mutually exclusive",
+              file=sys.stderr)
+        return 2
     if not args:
         print("At least one file or directory argument required.", file=sys.stderr)
         print("Use --help to show usage.", file=sys.stderr)
@@ -129,8 +156,17 @@ def main(args=None):
     else:
         requested = default_fixes
     fixer_names = requested.difference(unwanted_fixes)
-    rt = StdoutRefactoringTool(sorted(fixer_names), flags, sorted(explicit),
-                               options.nobackups, not options.no_diffs)
+
+    if options.unix_line_endings:
+        newline = '\n'
+    elif options.windows_line_endings:
+        newline = '\r\n'
+    else:
+        newline = None
+
+    rt = LineEndingsRefactoringTool(sorted(fixer_names), flags, sorted(explicit),
+                               options.nobackups, not options.no_diffs,
+                               newline=newline)
 
     # Refactor all files and directories passed as arguments
     if not rt.errors:
