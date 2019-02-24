5650894e684250b8dd7a7d552adeea98eb8663ba
Thomas Kluyver <thomas@kluyver.me.uk>
2019-02-24 12:35:36 +0000

    Version number -> 0.7

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2014, python-modernize team'
 # built documents.
 #
 # The short X.Y version.
-version = '0.6.1'
+version = '0.7'
 # The full version, including alpha/beta/rc tags.
 release = version
 
